7b6b89b899e03b6f82b6b25c62b0f14240fae2c4
Insidexa <nostresss77@gmail.com>
2018-12-03 18:15:55 +0200

    add notification service, one impl mail transport

---
+++ app/Services/MailTransport.js
@@ -0,0 +1,35 @@
+'use strict';
+
+const { ioc } = require('@adonisjs/fold');
+const nodemailer = require('nodemailer');
+
+class MailTransport {
+  constructor ({ email, password }) {
+    this.instance = nodemailer.createTransport(
+      `smtps://${email}:${password}@smtp.gmail.com`,
+    );
+  }
+
+  /**
+   *
+   * @param { NotificationDto } data
+   * @param { Function } onReceived
+   */
+  send (data, onReceived) {
+    const config = ioc.use('Adonis/Src/Config');
+    this.instance.sendMail({
+      from: config.get('mail.sender'), // sender address
+      to: data.to, // list of receivers
+      subject: data.subject, // Subject line
+      html: data.body, // text/plain body
+    }, (error, info) => {
+      if (error) {
+        return console.log(error);
+      }
+
+      return onReceived(info);
+    });
+  }
+}
+
+module.exports.MailTransport = MailTransport;
---
+++ app/Services/Notification.js
@@ -0,0 +1,22 @@
+'use strict';
+
+class Notification {
+  constructor (Transport) {
+    this.transport = Transport;
+  }
+
+  static factory (NotificationTransport) {
+    return new Notification(NotificationTransport);
+  }
+
+  /**
+   *
+   * @param { NotificationDto } data
+   * @param { Function } onReceived
+   */
+  send (data, onReceived) {
+    this.transport.send(data, onReceived);
+  }
+}
+
+module.exports.Notification = Notification;
