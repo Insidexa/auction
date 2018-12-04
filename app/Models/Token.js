1e1aa9764abe18d78a44d200d4d15dedaf8955f2
Insidexa <nostresss77@gmail.com>
2018-12-04 16:59:44 +0200

    add types of token

--- app/Models/Token.js
+++ app/Models/Token.js
@@ -3,7 +3,13 @@
 /** @type {typeof import('@adonisjs/lucid/src/Lucid/Model')} */
 const Model = use('Model');
 
+const CONFIRMATION_TOKEN = 'confirmation';
+
+const REMEMBER_TOKEN = 'remember';
+
 class Token extends Model {
 }
 
 module.exports = Token;
+module.exports.CONFIRMATION_TOKEN = CONFIRMATION_TOKEN;
+module.exports.REMEMBER_TOKEN = REMEMBER_TOKEN;
