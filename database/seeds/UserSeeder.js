19962368933df1fcacaaa2fd3974c76b1e457527
Insidexa <nostresss77@gmail.com>
2018-12-04 16:59:04 +0200

    cleanup database before new seeding

--- database/seeds/UserSeeder.js
+++ database/seeds/UserSeeder.js
@@ -12,9 +12,13 @@
 
 /** @type {import('@adonisjs/lucid/src/Factory')} */
 const Factory = use('Factory');
+const Database = use('Database');
 
 class UserSeeder {
   async run () {
+    await Database.table('tokens').delete();
+    await Database.table('users').delete();
+
     // https://eslint.org/docs/rules/no-plusplus
     for (let i = 0; i < 10; i++) {
       await Factory.model('App/Models/User').create();
