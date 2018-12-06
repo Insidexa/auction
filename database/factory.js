932401aa36891eb5315f5fc987dee32b462db583
Insidexa <nostresss77@gmail.com>
2018-12-06 14:04:12 +0200

    remove eslint type node

--- database/factory.js
+++ database/factory.js
@@ -15,23 +15,21 @@
 const Factory = use('Factory');
 
 Factory.blueprint('App/Models/User', (faker, i, data) => ({
-  email: data.email ? data.email : faker.email(),
+  email: faker.email(),
   phone: faker.phone(),
   first_name: faker.first(),
   lastname: faker.last(),
   birth_day: faker.date({ string: true }),
-  password: data.password ? data.password : faker.password(),
-  email_confirmed: data.email_confirmed ? data.email_confirmed : false,
+  password: faker.password(),
+  email_confirmed: false,
+  ...data,
 }));
 
 Factory.blueprint('App/Models/Lot', (faker, i, data) => ({
   title: faker.sentence({ words: 5 }),
   description: faker.paragraph(),
   image: '/images/example_image.jpg',
-  status: data.status,
-  user_id: data.user_id,
   current_price: 10.33,
   estimated_price: 2000,
-  start_time: data.start_time,
-  end_time: data.end_time,
+  ...data,
 }));
