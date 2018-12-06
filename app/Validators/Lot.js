dcf5e4b484fb6b53a313175709482274a4bd0520
Insidexa <nostresss77@gmail.com>
2018-12-06 10:21:08 +0200

    add lot validator and normalize dates in request to controDller

---
+++ app/Validators/Lot.js
@@ -0,0 +1,23 @@
+'use strict';
+
+class Lot {
+  get rules () {
+    return {
+      title: 'required|min:5',
+      current_price: 'required|min:0',
+      estimated_price: 'required|min:0',
+      status: 'required|integer',
+      start_time: 'required|date',
+      end_time: 'required|date',
+    };
+  }
+
+  get sanitizationRules () {
+    return {
+      start_time: 'toDate',
+      end_time: 'toDate',
+    };
+  }
+}
+
+module.exports = Lot;
