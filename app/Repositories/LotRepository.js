4cac15325329bdcbd7ccdbe0aa5dd5b2be085738
Insidexa <nostresss77@gmail.com>
2018-12-06 10:24:27 +0200

    fix filtering in my lots, add flag to check is my lot

--- app/Repositories/LotRepository.js
+++ app/Repositories/LotRepository.js
@@ -6,20 +6,39 @@ class LotRepository {
     this.perPage = 10;
   }
 
+  async all (filter) {
+    return await this.query
+      .inProcess()
+      .paginate(filter.page, this.perPage);
+  }
+
   /**
    * @param {LotFilterDto} filter
    * @param {User} user
    */
   async filter (filter, user) {
     const query = this.filterByType(this.query, filter, user);
+    const lots = await query.paginate(filter.page, this.perPage);
 
-    return await query.paginate(filter.page, this.perPage);
+    lots.rows = this.checkLotIsCurrentUser(lots.rows, user);
+
+    return lots;
+  }
+
+  checkLotIsCurrentUser (lots, user) {
+    return lots.map((lot) => {
+      if (lot.user_id === user.id) {
+        lot.self = true;
+      }
+
+      return lot;
+    });
   }
 
   filterByType (query, filter, user) {
     switch (filter.type) {
       case ALL:
-        return query;
+        return query.inProcess();
 
       case CREATED:
         return query.where('user_id', user.id);
