fbd23559e8a4ed41244cf0070bc3dfeb8abb243d
Insidexa <nostresss77@gmail.com>
2018-12-07 14:27:16 +0200

    one seeder for fill db because relations

--- database/seeds/AuctionTestDataSeeder.js
+++ database/seeds/AuctionTestDataSeeder.js
@@ -2,7 +2,7 @@
 
 /*
 |--------------------------------------------------------------------------
-| LotSeeder
+| AuctionTestDatumSeeder
 |--------------------------------------------------------------------------
 |
 | Make use of the Factory instance to seed database with dummy data or
@@ -14,19 +14,43 @@
 const Factory = use('Factory');
 const Database = use('Database');
 
-class LotSeeder {
+class AuctionTestDataSeeder {
   async run () {
+    await Database.table('tokens').delete();
+    await Database.table('orders').delete();
+    await Database.table('bids').delete();
     await Database.table('lots').delete();
-    const users = await Database.select('id').from('users');
+    await Database.table('users').delete();
 
-    users.forEach(async (user) => {
-      const current = new Date();
+    const [userForLots] = await this.makeUsers();
+    this.makeLots(userForLots);
+  }
 
-      const startTime = new Date();
-      startTime.setDate(current.getDate() + 10);
-      const endTime = new Date();
-      endTime.setDate(startTime.getDate() + 5);
+  async makeUsers () {
+    return [
+      await Factory.model('App/Models/User').create({
+        password: '12345678',
+        email: 'example1@g.com',
+        email_confirmed: true,
+      }),
+      await Factory.model('App/Models/User').create({
+        password: '12345678',
+        email: 'receiver@g.com',
+        email_confirmed: true,
+      }),
+    ];
+  }
 
+  async makeLots (user) {
+    const current = new Date();
+    const startTime = new Date();
+    startTime.setDate(current.getDate() + 10);
+    const endTime = new Date();
+    endTime.setDate(startTime.getDate() + 5);
+    const inProcessLotDate = new Date();
+    inProcessLotDate.setFullYear(2020);
+
+    return [
       await Factory.model('App/Models/Lot').create({
         user_id: user.id,
         status: 0,
@@ -34,11 +58,7 @@ class LotSeeder {
         estimated_price: 2000.33,
         start_time: startTime,
         end_time: endTime,
-      });
-
-      const inProcessLotDate = new Date();
-      inProcessLotDate.setFullYear(2020);
-
+      }),
       await Factory.model('App/Models/Lot').create({
         user_id: user.id,
         status: 1,
@@ -46,8 +66,7 @@ class LotSeeder {
         estimated_price: 1000.01,
         start_time: new Date(),
         end_time: inProcessLotDate,
-      });
-
+      }),
       await Factory.model('App/Models/Lot').create({
         user_id: user.id,
         status: 2,
@@ -55,9 +74,9 @@ class LotSeeder {
         estimated_price: 50000.20,
         start_time: new Date(),
         end_time: endTime,
-      });
-    });
+      }),
+    ];
   }
 }
 
-module.exports = LotSeeder;
+module.exports = AuctionTestDataSeeder;
