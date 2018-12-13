96cbf17b60eeddbe7fdca3f78aab187ff930da16
Insidexa <nostresss77@gmail.com>
2018-12-13 11:26:08 +0200

    add output linter errors

--- test/unit/linter-check.spec.js
+++ test/unit/linter-check.spec.js
@@ -8,6 +8,7 @@ test('linter', async ({ assert }) => {
   try {
     execSync('npm run eslint');
   } catch (error) {
+    console.log(error.stdout.toString());
     assert.isFalse(!!error);
   }
 }).timeout(0);
