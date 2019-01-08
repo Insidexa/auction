f9d7f3e7c0fc0443cc84b85c68e9e7e5d6af7c1e
Insidexa <nostresss77@gmail.com>
2019-01-08 21:49:22 +0200

    redis config

--- config/redis.js
+++ config/redis.js
@@ -31,14 +31,15 @@ module.exports = {
   | Configuration for a named connection.
   |
   */
-  local: {
-    host: '127.0.0.1',
-    port: Env.get('REDIS_PORT', 6379),
-    password: null,
+  local: 'redis://:1@redis-11382.c11.us-east-1-2.ec2.cloud.redislabs.com:11382',
+/*  local: {
+    host: Env.get('REDIS_HOST', '127.0.0.1'),
+    port: parseInt(Env.get('REDIS_PORT', 6379)),
+    password: Env.get('REDIS_PASSWORD'),
     db: 0,
     keyPrefix: '',
   },
-
+*/
   /*
   |--------------------------------------------------------------------------
   | cluster config
